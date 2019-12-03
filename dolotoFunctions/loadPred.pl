08bf19a6f1f56d8df5d37eba4e5e494c9269a460
DavidIMSER <d.medina@imserltda.com>
2019-12-03 08:10:54 -0300

    update repo

--- dolotoFunctions/loadPred.pl
+++ dolotoFunctions/loadPred.pl
@@ -1,49 +1,54 @@
 sub loadPred{
-	my $di = '';
+	my $di = '';#abre el archivo
 	open (F, "<$ff") or die "can't open $ff\n$!\n";
 	{ local $/=undef;  $di=<F>; }
-	my @d=split /[\r\n]+/, $di;
+	my @d=split /[\r\n]+/, $di;#quita el enter
 	close F;
 
-	my $cc = 0;
-	my $cc0 = 0;
-	my @nodes = ();
-	my %tfs = ();
-	$node = 0;
+	my $cc = 0;#contador
+	my $cc0 = 0;#contador
+	my @nodes = ();#array
+	my %tfs = ();#hash
+	$node = 0;#index
 
-	if($case == 1){
-		while(@d){
+	if($case == 1){#solo una red
+		while(@d){#recorre el array
 			if($d[0] =~ /\t/){
-				my @t =  split("\t",shift @d);
-				$tfs{$t[0]} = 1;
-				$tfall{$t[0]} = 1;
-				$nodeall{$t[0]} = 1;
-				$nodeall{$t[1]} = 1;
+				my @t =  split("\t",shift @d);#divide por \t
+				$tfs{$t[0]} = 1;#nodo1
+				$tfall{$t[0]} = 1;#nodo1
+				$nodeall{$t[0]} = 1;#nodo1
+				$nodeall{$t[1]} = 1;#nodo2
 
-				if(exists $net{"true"}{$t[0]}{$t[1]}){
-					if($t[2] > $threshold){
-						$net{"pred"}{$t[0]}{$t[1]} = 1;
-						$outnet{$t[0]}{$t[1]}[1] = 'P';
-						if(!defined $outnetnodes{$t[0]}){
+				#$outnetnodes tiene tres pocisiones en sus correspondientes keys!!!
+
+				if(exists $net{"true"}{$t[0]}{$t[1]}){#si existe el hash nodo1 y nodo2
+					if($t[2] > $threshold){#superar el umbral
+						$net{"pred"}{$t[0]}{$t[1]} = 1;#update pred de la clave nodo1-nodo2
+						$outnet{$t[0]}{$t[1]}[1] = 'P';#uodate dic de outnet[nodo1][nodo2]
+						if(!defined $outnetnodes{$t[0]}){#solo si la key existe en forma de arreglo...
+							$outnetnodes{$t[0]}[0] = 'TF';#actualizamos la pocision [node1][0]
+						}
+						elsif($outnetnodes{$t[0]}[0] ne 'TF'){#modificamos los valores si se encuentra vacia o nula
 							$outnetnodes{$t[0]}[0] = 'TF';
 						}
-						elsif($outnetnodes{$t[0]}[0] ne 'TF'){
-							$outnetnodes{$t[0]}[0] = 'TF';
-						}
-						if(!defined $outnetnodes{$t[1]}){
+						if(!defined $outnetnodes{$t[1]}){#lo mismo pero para el nodo2
 							$outnetnodes{$t[1]}[0] = 'nTF';
 						}
-						$outnetnodes{$t[0]}[2] = 'P';
+						$outnetnodes{$t[0]}[2] = 'P';#actualizamos la pocision dos del array en la key correspondiente .. OJO!!! ES POCISION 2!!!
 						$outnetnodes{$t[1]}[2] = 'P';
-						if($outnetnodes{$t[0]}[1] ne 'P'){
+						if($outnetnodes{$t[0]}[1] ne 'P'){#actualizar valor de node 1 en pos
 							$outnetnodes{$t[0]}[1] = 'A';
 						}
-						if($outnetnodes{$t[1]}[1] ne 'P'){
+						if($outnetnodes{$t[1]}[1] ne 'P'){#actualzamos los datos
 							$outnetnodes{$t[1]}[1] = 'A';
 						}
+						#actualizacion de los res
 						$res{"motNode"}{$t[0]}{"pred"} = {0,0,1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0,10,0,11,0,12,0,"rec",0,"tot",0};
 						$res{"motNode"}{$t[1]}{"pred"} = {0,0,1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0,9,0,10,0,11,0,12,0,"rec",0,"tot",0};
 						$cc++;
+
+						#preguntamos por el array y obtenemos la data y agregamos con push
 						if(!defined $inDegree{"pred"}{$t[1]}){
 							@{$inDegree{"pred"}{$t[1]}} = ();
 						}
